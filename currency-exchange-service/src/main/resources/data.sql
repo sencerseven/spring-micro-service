1df0d651a90cbf6c08f572d73496797c1b75975a
SENCER SEVEN <sencer.seven@consultant.turkcell.com.tr>
2019-07-01 10:04:54 +0300

    first blood

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO  exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values (10000,'USD','TL',65,8000);
+INSERT INTO  exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values (10001,'EURO','TL',75,8000);
+INSERT INTO  exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values (10002,'INR','TL',85,8000);
---
+++ currency-exchange-service8001/src/main/java/com/sencerseven/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -0,0 +1,17 @@
+package com.sencerseven.microservices.currencyexchangeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CurrencyExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+
+
+}
